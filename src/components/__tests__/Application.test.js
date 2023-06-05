5ef5a863e36368e8b50d715baaa86b3b254f1253
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-06-05 13:55:14 -0400

    adding tests for adding an appointment

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,70 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+import React from "react";
+
+/*
+  We import our helper functions from the react-testing-library (to enable us do our queries)
+  The render function allows us to render Components
+*/
+import {
+  getByText,
+  render,
+  waitForElement,
+  fireEvent,
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+} from "@testing-library/react";
+
+/*
+  We import the component that we are testing
+*/
+import Application from "components/Application";
+
+/*
+  A test that renders a React Component
+*/
+xit("renders without crashing", () => {
+  render(<Application />);
+});
+
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
+  // making an async call using async/await.
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // const { getyByText } = render(<Application />);
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // console.log(prettyDOM(container));
+
+    // gets all appointments in the container
+    const appointments = getAllByTestId(container, "appointment");
+    // console.log(prettyDOM(appointments));
+
+    // gets the first item in the container
+    // const appointment = getAllByTestId(container, "appointment")[0];
+    const appointment = appointments[0];
+    // console.log(prettyDOM(appointment));
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+  });
+});
